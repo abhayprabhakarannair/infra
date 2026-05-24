1284fc28c76c21883f8ae10fe16ec065f2905ebc
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-05-24 19:14:20 +0530

    Added fixes to vivaldi -> KDE bridge

--- home/desktop/gui.nix
+++ home/desktop/gui.nix
@@ -7,11 +7,6 @@
 
   # --- Essential apps ---
   home.packages = with pkgs.unstable; [
-    (vivaldi.override {
-      enableWidevine = true;
-      proprietaryCodecs = true;
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-    })
     bitwarden-desktop
     vlc
     fastfetch
@@ -19,6 +14,4 @@
     wl-clipboard
   ];
 
-  # --- Fix vivaldi to KDE bridge ---
-  home.file.".config/vivaldi/NativeMessagingHosts/org.kde.plasma.browser_integration.json".source = "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
 }
