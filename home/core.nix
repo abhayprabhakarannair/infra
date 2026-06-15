d36b7ad7a0309be881a3010352fc03b2ab39e9ad
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-06-15 23:47:19 +0530

    updated home

--- home/core.nix
+++ home/core.nix
@@ -12,5 +12,60 @@
     };
   };
 
+  programs.ssh = {
+   enable = true;
+   enableDefaultConfig = false;
+
+  includes = [
+    "/run/secrets/ssh-secret-ips"
+  ];
+
+  settings = {
+    "*" = {
+      ServerAliveInterval = 60;
+    };
+
+    "homelab-one" = {
+      User = "abhay";
+      Port = 2442;
+      IdentityFile = "/run/secrets/ssh-private-keys/homelab";
+      IdentitiesOnly = "yes";
+    };
+
+    "old-devil" = {
+      User = "abhay";
+      Port = 2442;
+      IdentityFile = "/run/secrets/ssh-private-keys/homelab";
+      IdentitiesOnly = "yes";
+    };
+
+    "daredevil" = {
+      User = "abhay";
+      Port = 2442;
+      IdentityFile = "/run/secrets/ssh-private-keys/homelab";
+      IdentitiesOnly = "yes";
+    };
+
+    "devil" = {
+      User = "abhay";
+      Port = 2442;
+      IdentityFile = "/run/secrets/ssh-private-keys/homelab";
+      IdentitiesOnly = "yes";
+    };
+
+    "homelab-storage-one" = {
+      IdentityFile = "/run/secrets/ssh-private-keys/homelab";
+      IdentitiesOnly = "yes";
+    };
+
+    "github.com" = {
+      User = "abhay";
+      IdentityFile = "/run/secrets/ssh-private-keys/github";
+      IdentitiesOnly = "yes";
+    };
+
+   };
+  };
+
   home.stateVersion = "26.05";
 }
