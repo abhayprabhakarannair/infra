6b328554d23e49a5d8fd35884b874b8d1277efc5
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-06-21 23:02:15 +0530

    Fixing forgejo side - gpg v6

--- modules/desktop/controlroom.nix
+++ modules/desktop/controlroom.nix
@@ -27,6 +27,9 @@
   environment.etc."git/allowed_signers".text = ''
     # Personal Devices
     abhayprabhakarannair@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0W8o0Bw9wa67ymsVVpspRXsFPcAk5yl9wFlZKecXpC
+
+    # Forgejo UI commits
+    noreply@git.iamabhay.fyi ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqx32jYbfgpOJY9k3LP2sCkFGiNm6IJ5uY6kDmRUGAG
   '';
 
 }
