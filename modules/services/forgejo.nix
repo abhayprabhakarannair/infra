5b3c34488c1cd5d46bfbbfc741e7e0cbe82eb9a6
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-06-21 22:28:39 +0530

    Fixing forgejo side

--- modules/services/forgejo.nix
+++ modules/services/forgejo.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  sops.secrets."ssh-private-keys/forgejo-server-key" = {
+    mode = "0440"; 
+  };
+
   systemd.tmpfiles.rules = [
     "d /srv/forgejo 0750 1000 1000 -"
   ];
@@ -12,10 +16,16 @@
     environment = {
       USER_UID = "1000"; 
       USER_GID = "1000";
-      FORGEJO__server__SSH_PORT = "2222"; 
       TZ = "Asia/Kolkata";
+      FORGEJO__server__SSH_PORT = "2222"; 
+      "FORGEJO__repository.signing__SIGNING_KEY" = "/data/forgejo-server-key";
+      "FORGEJO__repository.signing__SIGNING_NAME" = "Forge";
+      "FORGEJO__repository.signing__SIGNING_EMAIL" = "noreply@git.iamabhay.fyi";
+      "FORGEJO__repository.signing__MERGES" = "always";
+      "FORGEJO__repository.signing__CRUD_ACTIONS" = "always";
+      "FORGEJO__git.config__gpg.format" = "ssh";
     };
-    
+
     ports = [
       "127.0.0.1:3333:3000" 
       "2222:22"
@@ -24,6 +34,7 @@
     volumes = [
       "/srv/forgejo:/data"
       "/etc/localtime:/etc/localtime:ro"
+      "/run/secrets/ssh-private-keys/forgejo-server-key:/data/forgejo-server-key:ro"
     ];
     
     extraOptions = [
