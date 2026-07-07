fa0f05a66197c0447b096a19b2f7e6911cfce167
abhayprabhakarannair <abhayprabhakarannair@gmail.com>
2026-07-07 10:07:47 +0530

    Reworked WSL

--- home/desktop/wsl.nix
+++ home/desktop/wsl.nix
@@ -1,8 +1,91 @@
-{inputs, ...}: {
-  imports = [
-    "${inputs.self}/home/desktop"
-  ];
+{inputs, config, ...}: {
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Abhay Nair";
+        email = "anair@korewireless.com";
+	signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519_github.pub";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      core = {
+        excludesFileShow = "more";
+      };
+      gpg = {
+        format = "ssh";
+	ssh = {
+          allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+        };
+      };
+      commit = {
+        gpgsign = true;
+      };
+    };
+  };
+
+  # --- SSH ---
+  programs.ssh = {
+   enable = true;
+   enableDefaultConfig = false;
+
+   settings = {
+    "*" = {
+      ServerAliveInterval = 60;
+    };
+
+    "gitlab.com" = {
+      User = "git";
+      identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519_github";
+      IdentitiesOnly = "yes";
+      AddKeysToAgent = "yes";
+    };
+
+   };
+  };
+
+
+  # --- FISH Configuration ---
+  programs.fish = {
+    enable = true;
+
+    functions = {
+      fish_prompt = ''
+        set_color cyan
+        echo -n (prompt_pwd)
+        set_color normal
+        echo -n " => "
+      '';
+    };
+
+    shellAliases = {
+      ll = "ls -larth";
+      hrs = "home-manager switch --flake ~/Projects/infra#$(whoami)@$(cat /etc/hostname)";
+      ve = "nvim .";
+    };
+
+    interactiveShellInit = ''
+      set -g fish_greeting
+    '';
+  };
+
+  # --- DIR Env ---
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true; 
+    
+    config = {
+      global = {
+        hide_env_diff = true;
+      };
+    };
+  };
 
   home.username = "abhay";
   home.homeDirectory = "/home/abhay";
+
+
+  home.stateVersion = "26.05";
 }
